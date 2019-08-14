fc5f1a75960a32bde6c7d6d8410360591b0f4b95
qadams <qadams@unomaha.edu>
2019-08-14 17:40:00 -0500

    Files wanted

--- HealerPlugin/src/me/quinn/healerplugin/Main.java
+++ HealerPlugin/src/me/quinn/healerplugin/Main.java
@@ -1,14 +1,10 @@
 package me.quinn.healerplugin;
 
-import org.bukkit.Bukkit;
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 	@Override 
 	public void onEnable() {
 		getCommand("hbook").setExecutor(new CommandHwand());
-		Listener myEvtHnd = (Listener) new CommandHwand();
-        Bukkit.getPluginManager().registerEvents( myEvtHnd, this );
 	}
 }
